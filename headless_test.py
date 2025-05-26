3217abcd8cb48e6b173079f1d7de8b3d5dbd64f9
RayJay17787 <134681232+RayJay17787@users.noreply.github.com>
2025-05-26 08:07:42 -0700

    Create headless_test.py

---
+++ headless_test.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import time
+
+options = Options()
+options.headless = True
+
+# Change the URL to your web app (localhost or your deployed link)
+url = 'http://localhost:3000'
+
+# Start Firefox in headless mode
+driver = webdriver.Firefox(options=options)
+driver.get(url)
+
+print("Opened", url)
+time.sleep(5)
+print("Closing browser now.")
+
+driver.quit()
