b1e7eae8eae84d09dd1f62b8ddab13dcb8772e41
RayJay17787 <134681232+RayJay17787@users.noreply.github.com>
2025-05-26 10:49:55 -0700

    Update headless_test.py

--- headless_test.py
+++ headless_test.py
@@ -1,14 +1,32 @@
+#from selenium import webdriver
+#from selenium.webdriver.firefox.options import Options
+#import time
+
+#options = Options()
+#options.headless = True
+
+# Change the URL to your web app (localhost or your deployed link)
+#url = 'http://localhost:3000'
+
+# Start Firefox in headless mode
+#driver = webdriver.Firefox(options=options)
+#driver.get(url)
+
+#print("Opened", url)
+#time.sleep(5)
+#print("Closing browser now.")
+
+#driver.quit()
+
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import time
 
 options = Options()
-options.headless = False
+# options.headless = True  # <-- Make sure this line is removed or commented out
 
-# Change the URL to your web app (localhost or your deployed link)
 url = 'http://localhost:3000'
 
-# Start Firefox in headless mode
 driver = webdriver.Firefox(options=options)
 driver.get(url)
 
@@ -17,3 +35,4 @@ time.sleep(5)
 print("Closing browser now.")
 
 driver.quit()
+
