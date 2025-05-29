116469ae4d41cb5dfd2ad10f87ac20266ae521ae
RayJay17787 <134681232+RayJay17787@users.noreply.github.com>
2025-05-29 07:27:36 -0700

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiBaseUrl: process.env.REACT_APP_API_URL || 'http://localhost:5000'
+  apiBaseUrl: process.env.REACT_APP_API_URL && 'http://localhost:5000'
 };
 
 export default config;
